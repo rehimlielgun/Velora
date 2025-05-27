3859a59c271116d9aed670bf2d28943e3d14acdc
rehimlielgun <rehimlielgun8@gmail.com>
2025-05-27 06:15:06 +0400

    Create Home.js

---
+++ pages/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Xoş gəlmisiniz Velora saytına!</h1>
+      <p>Ana səhifədir. Burada qeydiyyat və giriş edə bilərsiniz.</p>
+    </div>
+  );
+}
